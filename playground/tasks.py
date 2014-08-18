37a0071293b40dd384113092c047dd10d91fbd41
Edoardo Rossi <edd.rossi@gmail.com>
2014-08-18 18:32:58 +0200

    Testing code.

--- playground/tasks.py
+++ playground/tasks.py
@@ -1,19 +1,32 @@
 from __future__ import absolute_import
 from celery import shared_task
+from celery.utils.log import get_task_logger
+import time
 
 __author__ = 'e.rossi'
 
 
-@shared_task
+logger = get_task_logger(__name__)
+
+@shared_task(name='tasks.add', serializer='json')
 def add(x, y):
+    logger.info('Adding {0} + {1}'.format(x, y))
     return x + y
 
 
-@shared_task
+@shared_task(name='tasks.slow_add', serializer='json')
+def slow_add(x, y):
+    d = 30
+    time.sleep(d)
+    logger.info('Adding {0} + {1} with delay {2}'.format(x, y, d))
+    return x + y
+
+
+@shared_task(serializer='json')
 def mul(x, y):
     return x * y
 
 
-@shared_task
+@shared_task(serializer='json')
 def xsum(numbers):
     return sum(numbers)
--- playground/tests.py
+++ playground/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class SimpleTest(TestCase):
+    """
+    This file demonstrates writing tests using the unittest module. These will pass
+    when you run "manage.py test".
+    Replace this with more appropriate tests for your application.
+    """
+
+    def test_basic_addition(self):
+        """
+        Tests that 1 + 1 always equals 2.
+        """
+        self.assertEqual(1 + 1, 2)
