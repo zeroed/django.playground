e2900cca0197a8e0384d5e5e6c6bd12478a10c0d
Edoardo Rossi <edd.rossi@gmail.com>
2014-08-19 17:04:42 +0200

    Naming tasks
    Better naming for tasks...

--- playground/tasks.py
+++ playground/tasks.py
@@ -8,25 +8,28 @@ __author__ = 'e.rossi'
 
 logger = get_task_logger(__name__)
 
-@shared_task(name='tasks.add', serializer='json')
+@shared_task(name='playground.tasks.add', serializer='json')
 def add(x, y):
     logger.info('Adding {0} + {1}'.format(x, y))
     return x + y
 
 
-@shared_task(name='tasks.slow_add', serializer='json')
+@shared_task(name='playground.tasks.slow_add', serializer='json')
 def slow_add(x, y):
-    d = 30
-    time.sleep(d)
-    logger.info('Adding {0} + {1} with delay {2}'.format(x, y, d))
-    return x + y
+    try:
+        d = 30
+        time.sleep(d)
+        logger.info('Adding {0} + {1} with delay {2}'.format(x, y, d))
+        return x + y
+    except Exception as exc:
+        slow_add.retry(exc=exc, countdown=10)
 
 
-@shared_task(serializer='json')
+@shared_task(name='playground.tasks.mul', serializer='json')
 def mul(x, y):
     return x * y
 
 
-@shared_task(serializer='json')
+@shared_task(name='playground.tasks.xsum', serializer='json')
 def xsum(numbers):
     return sum(numbers)
