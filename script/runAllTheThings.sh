caa655357f1dce66cbe52a64751aeb3bc7897ba1
Edoardo Rossi <edd.rossi@gmail.com>
2014-08-20 12:54:53 +0200

    Start all the things!

---
+++ script/runAllTheThings.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+cd ~/Workspace/django.playground
+
+# eddie@linuxbox:~/Workspace/django.playground$
+python3 manage.py runserver
+
+# eddie@linuxbox:~/Workspace/django.playground$
+python3 manage.py celery --app=foobar worker -l INFO --concurrency=5 --events --pidfile=pids/celerd.pid --logfile=logs/celeryd.log --broker='amqp://guest:guest@localhost:5672//' --heartbeat-interval=5 --detach
+
+# eddie@linuxbox:~/Workspace/django.playground$
+# tail -f logs/celery.log
+
+# eddie@linuxbox:~/Workspace/django.playground$
+python3 manage.py celerycam --verbosity=3 --frequency=1 --loglevel=INFO --broker='amqp://guest:guest@localhost:5672//' --pidfile=pids/celeryev.pid --logfile=logs/celeryev.log --detach
+
+# eddie@linuxbox:~/Workspace/django.playground$
+python3 manage.py celery beat -l INFO --app=foobar --broker='amqp://guest:guest@localhost:5672//' --max-interval=1 --pidfile=pids/celerybeat.pid --logfile=logs/celerybeat.log --detach
