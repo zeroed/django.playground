4af8dfe354921251e034273fb9c20df6703a939d
Edoardo Rossi <edd.rossi@gmail.com>
2014-08-20 18:48:26 +0200

    Register Taskmeta(s)

--- playground/admin.py
+++ playground/admin.py
@@ -3,6 +3,8 @@ from django.contrib import admin
 from django.contrib import admin
 from playground.models import Detector
 from playground.models import Result
+from playground.models import Taskmeta
 
 admin.site.register(Detector)
 admin.site.register(Result)
+admin.site.register(Taskmeta)
