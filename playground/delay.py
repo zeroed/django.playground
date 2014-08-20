bddc52e6cfb1d12b1a352f089c45c2de7af7f914
Edoardo Rossi <edd.rossi@gmail.com>
2014-08-20 11:40:26 +0200

    Add some randomness

--- playground/delay.py
+++ playground/delay.py
@@ -1,7 +1,8 @@
 from playground.tasks import slow_add, add
+import random
 
 __author__ = 'e.rossi'
 
-for counter in range(3):
-    result = slow_add.delay(10, 1)
+for counter in range(7):
+    result = slow_add.delay(random.randint(10, 100), random.randint(10, 100))
     print(result)
