b74143b0d7af5b2cde80ea0b219d421036bf106a
Edoardo Rossi <edd.rossi@gmail.com>
2014-08-20 11:43:11 +0200

    Add more randomness
    SQUASH ME, please Oh My GitGod!
    :monkey_face:

--- playground/tasks.py
+++ playground/tasks.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import
 from celery import shared_task
 from celery.utils.log import get_task_logger
 import time
+import random
 
 __author__ = 'e.rossi'
 
@@ -17,7 +18,7 @@ def add(x, y):
 @shared_task(name='playground.tasks.slow_add', serializer='json')
 def slow_add(x, y):
     try:
-        d = 30
+        d = random.randint(1, 30)
         time.sleep(d)
         logger.info('Adding {0} + {1} with delay {2}'.format(x, y, d))
         return x + y
