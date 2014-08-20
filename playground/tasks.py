670df2763819648c15866c3c3558368a60c4a92e
Edoardo Rossi <edd.rossi@gmail.com>
2014-08-20 18:48:49 +0200

    Store some Results

--- playground/tasks.py
+++ playground/tasks.py
@@ -1,8 +1,11 @@
 from __future__ import absolute_import
-from celery import shared_task
-from celery.utils.log import get_task_logger
 import time
 import random
+import datetime
+from celery import shared_task
+from celery.utils.log import get_task_logger
+from playground.models import Result
+from playground.models import Detector
 
 __author__ = 'e.rossi'
 
@@ -21,7 +24,13 @@ def slow_add(x, y):
         d = random.randint(1, 30)
         time.sleep(d)
         logger.info('Adding {0} + {1} with delay {2}'.format(x, y, d))
-        return x + y
+        result = Result()
+        result.content = "Meh... done I think"
+        result.value = x + y
+        result.detector = Detector.objects.get(id=1)
+        result.creation_date = datetime.datetime.now()
+        result.save()
+        return result.value
     except Exception as exc:
         slow_add.retry(exc=exc, countdown=10)
 
