312d88c42501ada92b35e03cba85204453b21e66
Edoardo Rossi <edd.rossi@gmail.com>
2014-08-21 12:55:41 +0200

    Start add something useful to the admin page

--- playground/admin.py
+++ playground/admin.py
@@ -5,6 +5,35 @@ from playground.models import Detector
 from playground.models import Result
 from playground.models import Taskmeta
 
-admin.site.register(Detector)
-admin.site.register(Result)
 admin.site.register(Taskmeta)
+
+"""
+Relevant Docs:
+https://docs.djangoproject.com/en/dev/ref/contrib/admin/
+"""
+
+def make_processed(modeladmin, request, queryset):
+    queryset.update(status='p')
+
+make_processed.short_description = "Mark selected result as processed"
+
+
+def upper_case_name(obj):
+    return ("%s" % (obj.name, )).upper()
+upper_case_name.short_description = 'Name'
+
+
+class DetectorAdmin(admin.ModelAdmin):
+    list_display = (upper_case_name, 'description', 'last_run_date', 'run_count')
+    readonly_fields = ('last_run_date', 'run_count', 'created_at')
+
+admin.site.register(Detector, DetectorAdmin)
+
+class ResultAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_at'
+    list_display = ['content', 'detector', 'value', 'duration']
+    ordering = ['created_at']
+    actions = [make_processed]
+    readonly_fields = list_display
+
+admin.site.register(Result, ResultAdmin)
