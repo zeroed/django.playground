96df4355a52ec5ec8e56eabe4f12d50eb1cbb1af
Edoardo Rossi <edd.rossi@gmail.com>
2014-08-26 17:33:59 +0200

    add stupid form

--- playground/forms.py
+++ playground/forms.py
@@ -1,1 +1,8 @@
+from django import forms
+
 __author__ = 'eddie'
+
+
+class RunTaskForm(forms.Form):
+    name = forms.CharField(max_length=100, label='Task Name', required=False)
+    number_of_tasks = forms.IntegerField(max_value=5, min_value=1, required=True)
