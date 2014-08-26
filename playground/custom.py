fbc5eb4c675d27c0de25a2f879f2e44518e92fbd
Edoardo Rossi <edd.rossi@gmail.com>
2014-08-26 17:33:14 +0200

    remove old file

---
+++ playground/custom.py
@@ -0,0 +1,19 @@
+from django.db import connection
+
+__author__ = 'eddie'
+
+
+def get_task_id_from_uuid(uuid):
+    """
+    Get the database_id of a task, given the UUID
+    https://docs.djangoproject.com/en/1.6/topics/db/sql/#connections-and-cursors
+    http://initd.org/psycopg/docs/usage.html#the-problem-with-the-query-parameters
+
+    :param uuid:
+    :return:
+    """
+    cursor = connection.cursor()
+    sql = "SELECT id, task_id FROM djcelery_taskstate WHERE task_id = %s;"
+    data = (uuid, )
+    cursor.execute(sql, data)
+    return cursor.fetchone()
