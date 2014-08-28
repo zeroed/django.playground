a7d2109ab1eb15e60f78b0fd5211f975d0115884
Edoardo Rossi <edd.rossi@gmail.com>
2014-08-28 18:53:41 +0200

    Add basic implementation

---
+++ playground/jobs/alpha.py
@@ -0,0 +1,29 @@
+import random
+from playground.jobs.sample import Sample
+from celery import shared_task
+from playground.tasks import slow_add
+
+__author__ = 'eddie'
+
+
+class Alpha(Sample):
+    """
+    Alpha Job Detector
+    """
+
+    @staticmethod
+    def name():
+        return "Alpha"
+
+    @staticmethod
+    def initialize():
+        return True
+
+    @staticmethod
+    @shared_task(name='playground.tasks.slow_add', serializer='json')
+    def run():
+        """
+        Just count and sleep
+        :return: True
+        """
+        return slow_add.apply_async((random.randint(10, 100), random.randint(10, 100)), countdown=2)
