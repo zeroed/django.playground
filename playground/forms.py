51fd1bdbc0cb3b6038b7a534c9bd3b3c20d7d470
Edoardo Rossi <edd.rossi@gmail.com>
2014-08-28 19:06:09 +0200

    Better forms

--- playground/forms.py
+++ playground/forms.py
@@ -3,6 +3,10 @@ from django import forms
 __author__ = 'eddie'
 
 
-class RunTaskForm(forms.Form):
+class RunTestTaskForm(forms.Form):
     name = forms.CharField(max_length=100, label='Task Name', required=False)
     number_of_tasks = forms.IntegerField(max_value=5, min_value=1, required=True)
+
+
+class RunDetectorTaskForm(forms.Form):
+    detector_name = forms.CharField(max_length=100, label='Detector Name', required=True)
