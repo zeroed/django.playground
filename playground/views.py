41c5ff3874db2ba9af94b22cd60d68112291b2c4
Edoardo Rossi <edd.rossi@gmail.com>
2014-08-28 19:09:08 +0200

    Add implementation for schedule runs.

--- playground/views.py
+++ playground/views.py
@@ -1,24 +1,18 @@
-import random
 import time
+import random
+from datetime import datetime
 from django.http import HttpResponse
 from django.template import RequestContext, loader
-from django.http import HttpResponseRedirect
-from datetime import datetime
-from celery.utils.log import get_task_logger
-from playground.forms import RunTaskForm
-from playground.tasks import slow_add
 from django.http import Http404
 from django.template import TemplateDoesNotExist
-from playground.custom import get_task_id_from_uuid
 from django.conf import settings
+from django.utils.html import escape
+from celery.utils.log import get_task_logger
+from playground.models import Detector
+from playground.tasks import slow_add
+from playground.forms import RunTestTaskForm, RunDetectorTaskForm
+from playground.custom import get_task_id_from_uuid
 
-# import tempfile import mkstemp
-# from os import fdopen, unlink, kill
-# from subprocess import Popen
-# import signal
-# import scripts
-
-# Create your core here.
 logger = get_task_logger(__name__)
 
 
@@ -44,13 +38,10 @@ def test(request):
 
 
 def test_schedule(request):
-    print("test_schedule")
     try:
         context_params = {}
         if request.method == 'POST':
-            form = RunTaskForm(request.POST)
-            print("received a POST form")
-            print(form)
+            form = RunTestTaskForm(request.POST)
             number_of_tasks = int(form.cleaned_data['number_of_tasks'])
             if form.is_valid() and int(number_of_tasks) > 0 and number_of_tasks < 6:
                 tasks = uuids = persisted_ids = []
@@ -75,55 +66,30 @@ def test_schedule(request):
         raise Http404()
 
 
-# def showjob(request):
-#     """
-#     Show the last result of the running job.
-#     :param request:
-#     """
-#     if not request.session in ['job']:
-#         return HttpResponse('Not running a job. <a href="/startjob/">Start a new one?</a>')
-#     else:
-#         filename = request.session['jobfile']
-#         results = open(filename)
-#         lines = results.readlines()
-#         try:
-#             return HttpResponse('{0}<p><a href=\"/rmjob/\">Terminate?</a>'.format(lines[-1]))
-#         except:
-#             return HttpResponse('No results yet. <p><a href="/rmjob/">Terminate?</a>')
-#     return response
-#
-#
-# def rmjob(request):
-#     """
-#     Terminate the runining job.
-#     """
-#     if request.session in ['job']:
-#         job = request.session['job']
-#         filename = request.session['jobfile']
-#         try:
-#             # unix only
-#             kill(job, signal.SIGKILL)
-#             unlink(filename)
-#         except OSError as e:
-#             # probably the job has finished already
-#             print(e)
-#             pass
-#         del request.session['job']
-#         del request.session['jobfile']
-#     # start a new one
-#     return HttpResponseRedirect('/startjob/')
-#
-#
-# def startjob(request):
-#     """
-#     Start a new long running process unless already started.
-#     """
-#     if not request.session.has_key('job'):
-#         # create a temporary file to save the resuls
-#         outfd, outname = mkstemp()
-#         request.session['jobfile'] = outname
-#         outfile = fdopen(outfd, 'a+')
-#         proc = Popen("python myjob.py", shell = True, stdout = outfile)
-#         # remember pid to terminate the job later
-#         request.session['job'] = proc.pid
-#     return HttpResponse('A <a href="/showjob/">new job</a> has started.')
+def run_schedule(request):
+    try:
+        context_params = {}
+        if request.method == 'POST':
+            form = RunDetectorTaskForm(request.POST)
+            print(form)
+            detector_name = escape(form.cleaned_data['detector_name'])
+            if form.is_valid() and detector_name and len(detector_name) > 0:
+                db_object_detector = Detector.get_by_name(detector_name)
+                task = db_object_detector.get_implementation().run.apply_async((random.randint(10, 100), random.randint(10, 100)), countdown=0)
+
+                # Silly but it seems this is necessary to find the object on the DB
+                time.sleep(1)
+
+                db_object = get_task_id_from_uuid(task.id)
+                print('\n---------\n%s:%s:%s\n-------\n' % (task.id, db_object, task.state))
+                context_params = {
+                    'uuid': task.id,
+                    'persisted_task_id': db_object[0]
+                }
+            else:
+                print("Form errors %s " % form.errors)
+        context = RequestContext(request, context_params)
+        template = loader.get_template('playground/test_schedule.html')
+        return HttpResponse(template.render(context))
+    except TemplateDoesNotExist:
+        raise Http404()
