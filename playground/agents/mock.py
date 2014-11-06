1bc8c300f7c0b989412f9a5728f789e888d98ccd
Edoardo Rossi <edd.rossi@gmail.com>
2014-11-06 17:35:00 +0100

    Add logging

--- playground/agents/mock.py
+++ playground/agents/mock.py
@@ -1,3 +1,4 @@
+import logging
 from datetime import datetime
 from playground.agents.base import Base
 from playground.jobs.mock import Mock as MockJob
@@ -9,16 +10,23 @@ class Mock(Base):
     """
         Sample mock Agent
     """
+
+    # Get an instance of a logger
+    logger = logging.getLogger(__name__)
+
     def __init__(self):
         super().__init__()
-        self.name = "mock"
+        self.name = "Mock"
         self.description = "Mock agent"
+        Mock.logger.info("{0} initialized... {1}".format(self.name, self))
 
     def run(self):
         """
         Run a count Job
         :return:
         """
+        Mock.logger.info("{0} start working... ".format(self.name, self))
         self.run_date = datetime.now()
         result = MockJob.run()
+        Mock.logger.info("{0} worked on {1}. ".format(self.name, result))
         return result
