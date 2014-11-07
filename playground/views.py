ac15615c1040cdc7d31c4a65ab3ba51907e935df
Edoardo Rossi <edd.rossi@gmail.com>
2014-11-07 10:50:56 +0100

    Take out the trash from code

--- playground/views.py
+++ playground/views.py
@@ -35,36 +35,6 @@ def test(request):
     return HttpResponse(html)
 
 
-# DELETE_ME
-# def test_schedule(request):
-#     try:
-#         context_params = {}
-#         if request.method == 'POST':
-#             form = RunTestTaskForm(request.POST)
-#             number_of_tasks = int(form.cleaned_data['number_of_tasks'])
-#             if form.is_valid() and int(number_of_tasks) > 0 and number_of_tasks < 6:
-#                 tasks = uuids = persisted_ids = []
-#                 for counter in range(number_of_tasks):
-#                     task = slow_add.apply_async((random.randint(10, 100), random.randint(10, 100)), countdown=0)
-#                     time.sleep(1)
-#                     db_object = get_task_id_from_uuid(task.id)
-#                     tasks.append(tasks)
-#                     uuids.append(tasks.id)
-#                     persisted_ids.append(db_object[0])
-#                     print('\n---------\n%s:%s:%s\n-------\n' % (task.id, db_object, task.state))
-#                 context_params = {
-#                     'uuids': uuids,
-#                     'persisted_task_ids': persisted_ids
-#                 }
-#             else:
-#                 print("Form errors %s " % form.errors)
-#         context = RequestContext(request, context_params)
-#         template = loader.get_template('playground/test_schedule.html')
-#         return HttpResponse(template.render(context))
-#     except TemplateDoesNotExist:
-#         raise Http404()
-
-
 def run_schedule(request):
     try:
         context_params = {}
